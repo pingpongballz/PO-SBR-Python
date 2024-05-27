2860ce5d67d153523efc72acd066e982eafcd735
pingpongballz <74599812+pingpongballz@users.noreply.github.com>
2024-05-27 23:47:11 +0800

    Add files via upload

--- Test_RCS.py
+++ Test_RCS.py
@@ -4,12 +4,11 @@ import matplotlib.pyplot as plt
 import time
 
 #Initial Params
-#filename = "C:/Users/e0406331/Downloads/libigl-tutorial-data-master/cube.obj"
-filename = "geometries/dihedral.obj"
+filename = "geometries/plate.obj"
 alpha = 180 #0/180 for V pol; 90/270 for H pol
-phi = np.arange(30,150,0.5) #90
+phi = np.arange(45,135,0.1) #90
 theta = 90
-freq = 5.405e9 #np.arange(1e9,4e9,30e6)
+freq = 3e9 #np.arange(1e9,4e9,30e6)
 raysperlam = 3
         
 
@@ -27,5 +26,4 @@ for ang in phi:
 toc = time.time()
 print(str(toc-tic) + " seconds")
 plt.plot(phi, arr)
-plt.ylim(0,50)
 plt.show()
