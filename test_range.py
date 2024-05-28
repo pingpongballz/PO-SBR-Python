a3b23166f2adeaec432ba62196eedabaab0c3f68
pingpongballz <74599812+pingpongballz@users.noreply.github.com>
2024-05-28 23:49:57 +0800

    fixed some errors

--- test_range.py
+++ test_range.py
@@ -6,7 +6,7 @@ import time
 #Uncomment/comment geometry u wanna use
 #Dihedral at ORIGIN.
 #filename = "geometries/dihedral.obj"
-#Dihedral at -2m OFFSET.
+#Dihedral at 2m OFFSET.
 filename = "geometries/dihedral_offset.obj"
 
 #Initial Params.
@@ -21,20 +21,21 @@ v,f = PO.build(filename)
 RCS = lambda E_theta,E_phi: 10*np.log10(4*np.pi*np.abs(E_theta)**2)
 arr = []
 
+
 tic = time.time()
 for i in freq:
     lam = (3e8)/(i)
     k = 2*np.pi/lam
     E1, E2,r0 = PO.simulate(alpha, phi, theta, i, raysperlam, v, f)
     factor = ((4*np.pi)/(-1j*k)) #normalisation. See R. Bhalla ISAR ray tube method 
-    
-    arr.append(factor*E1)
+    arr.append(E1)
+     
 
 toc = time.time()
 print(str(toc-tic) + " seconds")
-arr_fft = np.fft.fftshift(np.fft.fft(arr))
+arr_fft = np.fft.ifftshift(np.fft.ifft(arr)) #get range profile
 plt.plot(np.linspace(-7.5,7.5,len(freq)),np.abs(arr_fft))
-plt.title("0m offset" if filename == "geometries/dihedral.obj" else "-2m offset")
+plt.title("0m offset" if filename == "geometries/dihedral.obj" else "2m offset")
 plt.show()
 
 
